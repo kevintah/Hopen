393114bf82339dcfc8538806e9d145711769b62d
Njokom <knjokom@calstatela.edu>
2023-04-04 10:34:07 -0700

    can now sign in and sign out with google

---
+++ app/src/main/java/com/example/hopen/GoogleSignInActivity.kt
@@ -0,0 +1,28 @@
+package com.example.hopen
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.hopen.MainActivity.Companion.EXTRA_NAME
+import com.example.hopen.databinding.ActivityGoogleSignInBinding
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
+
+class GoogleSignInActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityGoogleSignInBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityGoogleSignInBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.textDisplayName.text = intent.getStringExtra(EXTRA_NAME)
+        binding.logout.setOnClickListener {
+            Firebase.auth.signOut()
+
+            val intent = Intent(applicationContext, MainActivity::class.java)
+            startActivity(intent)
+        }
+    }
+}
